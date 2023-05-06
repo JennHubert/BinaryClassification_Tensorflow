b6b9f6e1405373c18270fe0ff28ab16ebb8fa521
JennHubert <jenn.hubert19@gmail.com>
2023-05-06 12:04:53 -0400

    changes

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 scaler = MinMaxScaler() # Initializing scaler
 X_train_scaled = scaler.fit_transform(X_train) # Scaling X_train
-X_test_scaled = scaler.fit_transform(X_test) # Scaling X_test
+X_test_scaled = scaler.transform(X_test) # Scaling X_test
 
 
 
